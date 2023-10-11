5f74d24423185eb810a9f068f838338b842f6a84
suzWaks <suzalwakhley@gmail.com>
2023-10-11 14:49:12 +0600

    admin home color change

--- lib/Drawer/aboutus.dart
+++ lib/Drawer/aboutus.dart
@@ -9,36 +9,46 @@ class _AboutUsPageState extends State<AboutUsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-Widget body() {
-  return Container(
+      appBar: AppBar(
+        title: Text('About Us Page'),
+      ),
+      body: body(),
+    );
+  }
+
+  Widget body() {
+    return Container(
       child: Column(
-    children: [
-      Container(
-        height: 170,
-        child: const Align(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'About Us',
-                style: TextStyle(
-                  color: Color(0xFF0028A8),
-                  fontSize: 33,
-                  fontWeight: FontWeight.w700,
-                  height: 1.0,
-                ),
+        children: [
+          Container(
+            height: 170,
+            child: const Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'About Us',
+                    style: TextStyle(
+                      color: Color(0xFF0028A8),
+                      fontSize: 33,
+                      fontWeight: FontWeight.w700,
+                      height: 1.0,
+                    ),
+                  ),
+                  Image(
+                    image: AssetImage('images/aboutLogo.jpeg'),
+                    width: 184,
+                    height: 176,
+                  )
+                ],
               ),
-              Image(
-                image: AssetImage('images/aboutLogo.jpeg'),
-                width: 184,
-                height: 176,
-              )
-            ],
+            ),
           ),
-        ),
+          // Add more widgets here if needed
+        ],
+      ),
     );
   }
 }
